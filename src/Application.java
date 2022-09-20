b4d45c34b267a640e37ab2200acad994fd1f5fd7
rnagao <nagao1234@poli.ufrj.br>
2022-09-20 09:58:27 +0100

    Added the version number

--- src/Application.java
+++ src/Application.java
@@ -1,6 +1,8 @@
+import java.security.spec.RSAOtherPrimeInfo;
+
 public class Application {
     public static void main(String args[]) {
         System.out.println("Hello World");
-
+        System.out.println("Version 1.0");
     }
 }
